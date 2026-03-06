8c807c01bc00dd4f31f7c1e95ad8a19ce6ddab12
herbaa <herbaasc@gmail.com>
2026-03-06 15:41:53 +0400

    init

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const { webpack } = require('next/dist/compiled/webpack/webpack');
+
+require('dotenv').config();
+
+module.exports = {
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+    
+    config.plugins.push(new webpack.DefinePlugin(env));
+    
+    return config;
+  }
+};
